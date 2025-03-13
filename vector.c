b1a3748a44b0c46caaec9a6b57a4a833146ee3fd
AlexSiu06 <mularalekx@yandex.ru>
2025-03-13 18:45:17 +0300

    Update vector.c

--- vector.c
+++ vector.c
@@ -4,12 +4,19 @@
 #include "compare.h"
 #include "destruct.h"
 
-void Create(Vector* vector, size_t size, size_t size_element, int (*compare)(const void*, const void*)) {
-  vector->size_element = size_element;
-  vector->data = calloc(size, size_element);
-  vector->size = size;
-  vector->capacity = size;
-  vector->compare = compare;
+void SetElement(Vector* vector, const void* data){
+    const char* copy_data = (const void*)data;
+    char* vector_data = (char*)data;
+     for (size_t i = 0; i < vector->type_info.size_element; ++i) {
+         vector_data[i + vector->type_info.size_element * vector->size] = copy_data[i];
+     }
+}
+
+void Create(Vector* vector, size_t capacity, TypeInfo type_info) {
+  vector->data = calloc(capacity, type_info.size_element);
+  vector->size = 0;
+  vector->capacity = capacity;
+  vector->type_info = type_info;
 }
 
 void Clear(Vector* vector) {
@@ -17,7 +24,6 @@ void Clear(Vector* vector) {
   vector->data = NULL;
   vector->size = 0;
   vector->capacity = 0;
-  vector->compare = NULL;
 }
 
 void* MyRealoc(void* data, size_t old_capacity, size_t new_capacity, size_t size_element);
@@ -35,59 +41,54 @@ void* MyRealoc(void* data, size_t old_capacity, size_t new_capacity, size_t size
 void PushBack(Vector* vector, const void* value) {
   if (vector->size == vector->capacity) {
     if (vector->capacity == 0) {
-      vector->data = MyRealoc(vector->data, 0, 1, vector->size_element);
+      vector->data = MyRealoc(vector->data, 0, 1, vector->type_info.size_element);
       vector->capacity = 1;
     } else {
-      vector->data = MyRealoc(vector->data, vector->capacity, vector->capacity * 2, vector->size_element);
+      vector->data = MyRealoc(vector->data, vector->capacity, vector->capacity * 2, vector->type_info.size_element);
       vector->capacity *= 2;
     }
   }
-  char* copy_data = vector->data;
-  const char* copy_value = value;
-  for (size_t i = 0; i < vector->size_element; ++i) {
-
-    copy_data[i + vector->size_element * vector->size] = copy_value[i];
-  }
+    SetElement(vector, value);
   ++vector->size;
 }
 
 Vector Copy(const Vector* other){
   Vector vector;
-    Create(&vector, other->size, other->size_element, other->compare);
+    Create(&vector, other->size, other->type_info);
   char* copy_data = vector.data;
   char* copy_other_data = other->data;
   for (size_t i = 0; i < other->size; ++i) {
-    char* data = copy_data + i * vector.size_element;
-    const char* other_data = copy_other_data + i * other->size_element;
-    for (size_t j = 0; j < other->size_element; ++j) {
+    char* data = copy_data + i * vector.type_info.size_element;
+    const char* other_data = copy_other_data + i * other->type_info.size_element;
+    for (size_t j = 0; j < other->type_info.size_element; ++j) {
       data[j] = other_data[j];
     }
   }
   return vector;
 }
 
-void PopBack(Vector* vector, void (*Destructor)(void*)){
+void PopBack(Vector* vector){
   --vector->size;
     char* copy_object = (char*)vector->data;
-    Destructor(copy_object + vector->size * vector->size_element);
+    vector->type_info.destruct(copy_object + vector->size * vector->type_info.size_element);
 }
 
 void* GetElement(Vector vector, size_t index) {
-    if(index < 0 || index >= vector.size){
+    if(index >= vector.size){
         return NULL;
     }
   return vector.data + index;
 }
 
 int Erase(Vector* vector, size_t index) {
-    if(index < 0 || index >= vector->size){
+    if(index >= vector->size){
         return 1;
     }
   char* copy_data = vector->data;
   for (size_t i = 0; i < vector->size; ++i) {
-    char* data = copy_data + i * vector->size_element;
-    char* next_data = copy_data + (i + 1) * vector->size_element;
-    for (size_t j = 0; j < vector->size_element; ++j) {
+    char* data = copy_data + i * vector->type_info.size_element;
+    char* next_data = copy_data + (i + 1) * vector->type_info.size_element;
+    for (size_t j = 0; j < vector->type_info.size_element; ++j) {
       data[j] = next_data[j];
     }
   }
@@ -96,70 +97,63 @@ int Erase(Vector* vector, size_t index) {
 }
 
 int Insert(Vector* vector, const void* value, size_t index) {
-    if(index < 0 || index >=vector->size){
+    if(index >=vector->size){
         return 1;
     }
     if (vector->size == vector->capacity) {
-    vector->data = MyRealoc(vector->data, vector->capacity, vector->capacity * 2, vector->size_element);
+    vector->data = MyRealoc(vector->data, vector->capacity, vector->capacity * 2, vector->type_info.size_element);
   }
   char* copy_data = vector->data;
   for (size_t i = 0; i < vector->size; ++i) {
-    char* data = copy_data + i * vector->size_element;
-    char* previos_data = copy_data + (i - 1) * vector->size_element;
-    for (size_t j = vector->size_element; j > index; --j) {
+    char* data = copy_data + i * vector->type_info.size_element;
+    char* previos_data = copy_data + (i - 1) * vector->type_info.size_element;
+    for (size_t j = vector->type_info.size_element; j > index; --j) {
       data[j] = previos_data[j];
     }
   }
   char* data = vector->data;
   const char* copy_value = value;
-  for (size_t i = 0; i < vector->size_element; ++i) {
-    data[i] = copy_value[i + i * vector->size_element];
+  for (size_t i = 0; i < vector->type_info.size_element; ++i) {
+    data[i] = copy_value[i + i * vector->type_info.size_element];
   }
     return 0;
 }
 
 Vector Where(const Vector* vector, int Predicate(void*)){
     Vector result;
-    Create(&result, vector->size, result.size_element, vector->compare);
+    Create(&result, vector->size, vector->type_info);
     char* copy_vector_data = vector->data;
     for(size_t i = 0; i < vector->size; ++i){
-        char* data = copy_vector_data + i * vector->size_element;
+        char* data = copy_vector_data + i * vector->type_info.size_element;
         if(Predicate(data) == 1){
-            memcpy(&result, data, 1);
+            PushBack(&result, data);
         }
     }
     return result;
 }
 
-Vector Map(size_t count_segment, size_t size) {
-    Vector result;
-    Create(&result, count_segment, sizeof(Segment), SegmentCompare);
-    size_t segment_size = size / count_segment;
-    size_t remainder = size % count_segment;
-    Segment* segments = (Segment*)result.data;
-    size_t start_index = 0;
-    for (size_t i = 0; i < count_segment; ++i) {
-        size_t end_index = start_index + segment_size + (i < remainder ? 1 : 0);
-        segments[i].start_index = start_index;
-        segments[i].end_index = end_index;
-        start_index = end_index;
+Vector Map(Vector* vector, void (*unary)(void*)) {
+    Vector result = Copy(vector);
+    char* copy_vector_data = (char*)result.data;
+    for(size_t i = 0; i < vector->size; ++i){
+        char* data = copy_vector_data + i * vector->type_info.size_element;
+        unary(data);
     }
-
     return result;
 }
 
 Vector Concatenate(const Vector* vector1, const Vector* vector2) {
     Vector result;
     size_t total_size = vector1->size + vector2->size;
-    Create(&result, total_size, vector1->size_element, vector1->compare);
+    Create(&result, total_size, vector1->type_info);
     char* result_data = (char*)result.data;
     char* data1 = (char*)vector1->data;
     for (size_t i = 0; i < vector1->size; ++i) {
-        memcpy(result_data + i * vector1->size_element, data1 + i * vector1->size_element, vector1->size_element);
+        memcpy(result_data + i * vector1->type_info.size_element, data1 + i * vector1->type_info.size_element, vector1->type_info.size_element);
     }
     char* data2 = (char*)vector2->data;
     for (size_t i = 0; i < vector2->size; ++i) {
-        memcpy(result_data + (vector1->size + i) * vector2->size_element, data2 + i * vector2->size_element,vector2->size_element);
+        memcpy(result_data + (vector1->size + i) * vector2->type_info.size_element, data2 + i * vector2->type_info.size_element,vector2->type_info.size_element);
     }
     return result;
 }
@@ -200,11 +194,11 @@ void MergeSortRec(void* data, size_t size, size_t size_element, int (*compare)(c
 }
 
 void Sort(Vector* vector){
-    if (vector->compare == NULL) {
-            return;
-        }
-    char* byfer = calloc(vector->size, vector->size_element);
-    MergeSortRec(vector->data, vector->size, vector->size_element, vector->compare, byfer);
-    free(byfer);
+    if (vector->type_info.compare == NULL) {
+        return;
+    }
+    char* buffer = calloc(vector->size, vector->type_info.size_element);
+    MergeSortRec(vector->data, vector->size, vector->type_info.size_element, vector->type_info.compare, buffer);
+    free(buffer);
 }
 
