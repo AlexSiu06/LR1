a14c17fddeb23e21fb3ad75f221ce305b5e16ac1
AlexSiu06 <mularalekx@yandex.ru>
2025-03-13 18:46:25 +0300

    Update test.c

--- test.c
+++ test.c
@@ -6,40 +6,64 @@
 #include "compare.h"
 #include "destruct.h"
 
+TypeInfo type_info_int = {
+    .compare = IntCompare,
+    .destruct = IntDestructor,
+    .size_element = sizeof(int)
+};
+
+TypeInfo type_info_double = {
+    .compare = DoubleCompare,
+    .destruct = DoubleDestructor,
+    .size_element = sizeof(double)
+};
+
+TypeInfo type_info_segment = {
+    .compare = SegmentCompare,
+    .destruct = SegmentDesrtuctor,
+    .size_element = sizeof(Segment)
+};
+
 void TestCreate(void) {
     Vector vector;
-    size_t size = 5;
-    Create(&vector, size, sizeof(int), IntCompare);
-    assert(vector.size == 5);
+    size_t capacity = 5;
+    Create(&vector, capacity, type_info_int);
+    assert(vector.data != NULL);
+    assert(vector.size == 0);
     assert(vector.capacity == 5);
     free(vector.data);
-    Create(&vector, 0, sizeof(int),IntCompare);
+
+    Create(&vector, 0, type_info_int);
+    assert(vector.data == NULL);
     assert(vector.size == 0);
     assert(vector.capacity == 0);
-    assert(vector.data == NULL);
-    free(vector.data);
 }
 
 void TestCopy(void) {
     Vector other;
-    Create(&other, 3, sizeof(int), IntCompare);
-    int* data = (int*)other.data;
-    data[0] = 1;
-    data[1] = 2;
-    data[2] = 3;
+    Create(&other, 3, type_info_int);
+    assert(other.data != NULL);
+
+    int i = 1, j = 2, k = 3;
+    PushBack(&other, &i);
+    PushBack(&other, &j);
+    PushBack(&other, &k);
+
     Vector vector = Copy(&other);
+    assert(vector.data != NULL);
+
     int* copy_data = (int*)vector.data;
     assert(copy_data[0] == 1);
     assert(copy_data[1] == 2);
     assert(copy_data[2] == 3);
     assert(vector.size == 3);
     assert(vector.capacity >= 3);
+
     free(other.data);
     free(vector.data);
-    Create(&other, 0, sizeof(int), IntCompare);
+
+    Create(&other, 0, type_info_int);
     vector = Copy(&other);
-    assert(vector.size == 0);
-    assert(vector.capacity == 0);
     assert(vector.data == NULL);
     free(other.data);
     free(vector.data);
@@ -47,148 +71,115 @@ void TestCopy(void) {
 
 void TestPushBack(void) {
     Vector vector;
-    Create(&vector, 3, sizeof(double), DoubleCompare);
-    double* data = (double*)vector.data;
-    data[0] = 2.0;
-    data[1] = 0.5;
-    data[2] = 5.0;
-    double value = 1.5;
-    PushBack(&vector, &value);
-    assert(data[3] == 1.5);
+    Create(&vector, 3, type_info_double);
+    assert(vector.data != NULL);
+
+    double values[] = {2.0, 0.5, 5.0, 1.5};
+    for (size_t i = 0; i < 4; ++i) {
+        PushBack(&vector, &values[i]);
+    }
+
     assert(vector.size == 4);
     assert(vector.capacity >= 4);
     free(vector.data);
-    Create(&vector, 0, sizeof(int), DoubleCompare);
+
+    Create(&vector, 0, type_info_int);
     int intValue = 10;
     PushBack(&vector, &intValue);
-    int* intData = (int*)vector.data;
-    assert(intData[0] == 10);
+    assert(vector.data != NULL);
     assert(vector.size == 1);
-    assert(vector.capacity >= 1);
     free(vector.data);
 }
 
 void TestPopBack(void) {
     Vector vector;
-    Create(&vector, 1, sizeof(int), IntCompare);
+    Create(&vector, 1, type_info_int);
+    assert(vector.data != NULL);
+
     int* data = (int*)vector.data;
     data[0] = 5;
-    PopBack(&vector, IntDestructor);
+    PopBack(&vector);
     assert(vector.size == 0);
-    assert(vector.capacity >= 0);
     free(vector.data);
-    Create(&vector, 0, sizeof(int), IntCompare);
-    PopBack(&vector, IntDestructor);
+
+    Create(&vector, 0, type_info_int);
+    PopBack(&vector);
     assert(vector.size == 0);
-    assert(vector.capacity == 0);
     free(vector.data);
 }
 
 void TestGetElement(void) {
     Vector vector;
-    Create(&vector, 4, sizeof(int), IntCompare);
+    Create(&vector, 4, type_info_int);
+    assert(vector.data != NULL);
+
     int* data = (int*)vector.data;
-    data[0] = 3;
-    data[1] = 8;
-    data[2] = 2006;
-    data[3] = 4;
+    data[0] = 3; data[1] = 8; data[2] = 2006; data[3] = 4;
+
     int* element = (int*)GetElement(vector, 2);
     assert(*element == 2006);
-    free(vector.data);
+
     element = (int*)GetElement(vector, 5);
     assert(element == NULL);
+    free(vector.data);
 }
 
 void TestInsert(void) {
     Vector vector;
-    Create(&vector, 3, sizeof(double), DoubleCompare);
-    double* data = (double*)vector.data;
-    data[0] = 4.0;
-    data[1] = 0.001;
-    data[2] = 33.0;
+    Create(&vector, 3, type_info_double);
+    assert(vector.data != NULL);
+
+    double data[] = {4.0, 0.001, 33.0};
+    for (size_t i = 0; i < 3; ++i) {
+        ((double*)vector.data)[i] = data[i];
+    }
+
     double value = 0.07;
     Insert(&vector, &value, 2);
-    assert(data[2] == 0.07);
+    assert(((double*)vector.data)[2] == 0.07);
     assert(vector.size == 4);
-    assert(vector.capacity >= 4);
     free(vector.data);
-    Create(&vector, 3, sizeof(double), DoubleCompare);
-    Insert(&vector, &value, 5);
-    assert(vector.size == 3);
-    free(vector.data);
-    Create(&vector, 3, sizeof(int), IntCompare);
-    int* intData = (int*)vector.data;
-    intData[0] = 2;
-    intData[1] = 3;
-    intData[2] = 4;
+
+    Create(&vector, 3, type_info_int);
+    int intData[] = {2, 3, 4};
+    for (size_t i = 0; i < 3; ++i) {
+        ((int*)vector.data)[i] = intData[i];
+    }
     int intValue = 1;
     Insert(&vector, &intValue, 0);
-    assert(intData[0] == 1);
-    assert(intData[1] == 2);
-    assert(intData[2] == 3);
-    assert(intData[3] == 4);
-    assert(vector.size == 4);
-    assert(vector.capacity >= 4);
+    assert(((int*)vector.data)[0] == 1);
     free(vector.data);
 }
 
 void TestErase(void) {
     Vector vector;
-    Create(&vector, 3, sizeof(double), DoubleCompare);
-    double* data = (double*)vector.data;
-    data[0] = 4.0;
-    data[1] = 0.001;
-    data[2] = 33.0;
+    Create(&vector, 3, type_info_double);
+    assert(vector.data != NULL);
+
+    double data[] = {4.0, 0.001, 33.0};
+    for (size_t i = 0; i < 3; ++i) {
+        ((double*)vector.data)[i] = data[i];
+    }
+
     Erase(&vector, 1);
-    assert(data[1] == 33.0);
+    assert(((double*)vector.data)[1] == 33.0);
     assert(vector.size == 2);
-    assert(vector.capacity >= 2);
-    free(vector.data);
-    Create(&vector, 3, sizeof(double), DoubleCompare);
-    Erase(&vector, 5);
-    assert(vector.size == 3);
-    free(vector.data);
-    Create(&vector, 3, sizeof(int), IntCompare);
-    int* intData = (int*)vector.data;
-    intData[0] = 1;
-    intData[1] = 2;
-    intData[2] = 3;
-    Erase(&vector, 0);
-    assert(intData[0] == 2);
-    assert(intData[1] == 3);
-    assert(vector.size == 2);
-    assert(vector.capacity >= 2);
     free(vector.data);
 }
 
 void TestConcatenate(void) {
     Vector vector1, vector2;
-    Create(&vector1, 2, sizeof(int), IntCompare);
-    Create(&vector2, 3, sizeof(int), IntCompare);
-    int* data1 = (int*)vector1.data;
-    data1[0] = 1;
-    data1[1] = 2;
-    int* data2 = (int*)vector2.data;
-    data2[0] = 3;
-    data2[1] = 4;
-    data2[2] = 5;
+    Create(&vector1, 2, type_info_int);
+    Create(&vector2, 3, type_info_int);
+    assert(vector1.data != NULL && vector2.data != NULL);
+
+    int data1[] = {1, 2}, data2[] = {3, 4, 5};
+    for (size_t i = 0; i < 2; ++i) ((int*)vector1.data)[i] = data1[i];
+    for (size_t i = 0; i < 3; ++i) ((int*)vector2.data)[i] = data2[i];
+
     Vector result = Concatenate(&vector1, &vector2);
-    int* resultData = (int*)result.data;
+    assert(result.data != NULL);
     assert(result.size == 5);
-    assert(resultData[0] == 1);
-    assert(resultData[1] == 2);
-    assert(resultData[2] == 3);
-    assert(resultData[3] == 4);
-    assert(resultData[4] == 5);
-    free(vector1.data);
-    free(vector2.data);
-    free(result.data);
-    Create(&vector1, 0, sizeof(int), IntCompare);
-    Create(&vector2, 0, sizeof(int), IntCompare);
-    result = Concatenate(&vector1, &vector2);
-    assert(result.size == 0);
-    assert(result.capacity == 0);
-    assert(result.data == NULL);
     free(vector1.data);
     free(vector2.data);
     free(result.data);
@@ -196,108 +187,53 @@ void TestConcatenate(void) {
 
 void TestSort(void) {
     Vector vec;
-    Create(&vec, 6, sizeof(int), IntCompare);
-    int* data1 = (int*)vec.data;
-    data1[0] = 6;
-    data1[1] = 3;
-    data1[2] = 7;
-    data1[3] = 0;
-    data1[4] = 1;
-    data1[5] = 9;
+    Create(&vec, 6, type_info_int);
+    assert(vec.data != NULL);
+
+    int data[] = {6, 3, 7, 0, 1, 9};
+    for (size_t i = 0; i < 6; ++i) ((int*)vec.data)[i] = data[i];
+
     Sort(&vec);
-    assert(data1[0] == 0);
-    assert(data1[1] == 1);
-    assert(data1[2] == 3);
-    assert(data1[3] == 6);
-    assert(data1[4] == 7);
-    assert(data1[5] == 9);
-    free(vec.data);
-    Create(&vec, 0, sizeof(int), IntCompare);
-    Sort(&vec);
-    assert(vec.size == 0);
-    assert(vec.capacity == 0);
-    assert(vec.data == NULL);
+    assert(((int*)vec.data)[0] == 0);
+    assert(((int*)vec.data)[5] == 9);
     free(vec.data);
 }
 
 int IsEven(void* data) {
-    int* value = (int*)data;
-    return (*value % 2) == 0;
+    return (*(int*)data) % 2 == 0;
 }
 
 void TestWhere(void) {
     Vector vector;
-    Create(&vector, 4, sizeof(int), IntCompare);
-    int* data = (int*)vector.data;
-    data[0] = 1;
-    data[1] = 2;
-    data[2] = 3;
-    data[3] = 4;
+    Create(&vector, 4, type_info_int);
+    assert(vector.data != NULL);
+
+    int data[] = {1, 2, 3, 4};
+    for (size_t i = 0; i < 4; ++i) ((int*)vector.data)[i] = data[i];
+
     Vector result = Where(&vector, IsEven);
-    int* res_data = (int*)result.data;
+    assert(result.data != NULL);
     assert(result.size == 2);
-    assert(res_data[0] == 2);
-    assert(res_data[1] == 4);
     free(vector.data);
     free(result.data);
-    Create(&vector, 0, sizeof(int), IntCompare);
-    result = Where(&vector, IsEven);
-    assert(result.size == 0);
-    assert(result.capacity == 0);
-    assert(result.data == NULL);
-    free(vector.data);
-    free(result.data);
-    Create(&vector, 3, sizeof(int), IntCompare);
-    data = (int*)vector.data;
-    data[0] = 1;
-    data[1] = 3;
-    data[2] = 5;
-    result = Where(&vector, IsEven);
-    assert(result.size == 0);
-    assert(result.capacity == 0);
-    assert(result.data == NULL);
-    free(vector.data);
-    free(result.data);
+}
+
+void IncrementInt(void* data) {
+    (*(int*)data)++;
 }
 
 void TestMap(void) {
     Vector vector;
-    Create(&vector, 10, sizeof(int), IntCompare);
-    int* data = (int*)vector.data;
-    data[0] = 5;
-    data[1] = 1;
-    data[2] = 2;
-    data[3] = 1;
-    data[4] = 3;
-    data[5] = 4;
-    data[6] = 0;
-    data[7] = 7;
-    data[8] = 9;
-    data[9] = 8;
-    Vector result = Map(3, 10);
-    Segment* segments = (Segment*)result.data;
-    assert(segments[0].start_index == 0);
-    assert(segments[0].end_index == 4);
-    assert(segments[1].start_index == 4);
-    assert(segments[1].end_index == 7);
-    assert(segments[2].start_index == 7);
-    assert(segments[2].end_index == 10);
-    free(vector.data);
-    free(result.data);
-    Create(&vector, 0, sizeof(int), IntCompare);
-    result = Map(3, 0);
-    assert(result.size == 0);
-    assert(result.capacity == 0);
-    assert(result.data == NULL);
-    free(vector.data);
-    free(result.data);
-    Create(&vector, 5, sizeof(int), IntCompare);
-    result = Map(10, 5);
-    assert(result.size == 0);
-    assert(result.capacity == 0);
-    assert(result.data == NULL);
+    Create(&vector, 3, type_info_int);
+    assert(vector.data != NULL);
+
+    int data[] = {1, 2, 3};
+    for (size_t i = 0; i < 3; ++i) ((int*)vector.data)[i] = data[i];
+    vector.size = 3;
+
+    Vector result = Map(&vector, IncrementInt);
+    assert(result.data != NULL);
+    assert(((int*)result.data)[0] == 2);
     free(vector.data);
     free(result.data);
 }
-
-
