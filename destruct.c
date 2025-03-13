c0ff31c47161c475ee63e63d37bd0bbf02474988
AlexSiu06 <mularalekx@yandex.ru>
2025-03-13 18:47:41 +0300

    Update destruct.c

--- destruct.c
+++ destruct.c
@@ -15,6 +15,12 @@ void DoubleDestructor(void* object){
 
 void VectorDestructor(void* vector){
     Vector* copy_vector = (Vector*)vector;
+    char* copy_data = (char*)copy_vector->data;
+    for(size_t i = 0; i < copy_vector->size; ++i){
+        copy_vector->type_info.destruct(copy_data + i * copy_vector->type_info.size_element);
+    }
     free(copy_vector->data);
     copy_vector->data = NULL;
 }
+
+void SegmentDesrtuctor(void* Segment){}
